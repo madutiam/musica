28b00f5b24079bef1fb90182fe0b43a28cc46f98
Maria Eduarda Bedetti Amorim <conta.para.qualquer.coisa2005@gmail.com>
2022-03-13 01:15:04 -0300

    finalização

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"Até que durou");
+Console.ResetColor();
 Console.WriteLine("");
 Console.WriteLine(@"Tentei fazer valer a pena
 Passei por cima dos problemas
@@ -32,22 +35,22 @@ Te faço um favor, melhor eu sumir");
 
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine(@"Fui mais um nas suas mãos
-                    Não sei se por carência ou falta de opção
-                    Só sei que eu senti amor
-                    Que pena, só eu senti amor
-                    O que faço com os planos dos próximos anos
-                    Juro que não sei
-                    Só sei que eu senti amor
-                    Que pena, só eu senti amor
+Não sei se por carência ou falta de opção
+Só sei que eu senti amor
+Que pena, só eu senti amor
+O que faço com os planos dos próximos anos
+Juro que não sei
+Só sei que eu senti amor
+Que pena, só eu senti amor
 
-                    Fui mais um nas suas mãos
-                    Não sei se por carência ou falta de opção
-                    Só sei que eu senti amor
-                    Que pena, só eu senti amor
-                    O que faço com os planos dos próximos anos
-                    Juro que não sei
-                    Só sei que eu senti amor
-                    Que pena, só eu senti amor
-                    Só eu (Só eu) Senti amor");
+Fui mais um nas suas mãos
+Não sei se por carência ou falta de opção
+Só sei que eu senti amor
+Que pena, só eu senti amor
+O que faço com os planos dos próximos anos
+Juro que não sei
+Só sei que eu senti amor
+Que pena, só eu senti amor
+Só eu (Só eu) Senti amor");
 Console.ResetColor();
 Console.WriteLine("");
